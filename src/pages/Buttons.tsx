97e7fde7d97952241e8d79c902b51101c6f476a9
Vlad <vladslantsitskiy@gmail.com>
2022-05-18 23:06:10 +0300

    bug fixed

--- src/pages/Buttons.tsx
+++ src/pages/Buttons.tsx
@@ -5,37 +5,37 @@ import { useSelector } from 'react-redux'
 function Buttons() {
 
     enum type {
-       symbol = 'symbol',
-       operator = 'operator',
-       special = 'special',
-      }
+        numbers = 'numbers',
+        operator = 'operator',
+        special = 'special',
+    }
 
     const [buttons, setButtons] = useState([
-    { id: 1, value: 'ac', type: type.special },
-    { id: 2, value: '<', type: type.special },
-    { id: 3, value: '/', type: type.operator  },
-    { id: 4, value: 'x', type: type.operator },
-    { id: 5, value: '7', type: type.symbol },
-    { id: 6, value: '8', type: type.symbol  },
-    { id: 7, value: '9', type: type.symbol  },
-    { id: 8, value: '-', type: type.operator },
-    { id: 9, value: '4', type: type.symbol  },
-    { id: 10, value: '5', type: type.symbol  },
-    { id: 11, value: '6', type: type.symbol  },
-    { id: 12, value: '+', type: type.operator },
-    { id: 13, value: '1', type: type.symbol  },
-    { id: 14, value: '2', type: type.symbol  },
-    { id: 15, value: '3', type: type.symbol  },
-    { id: 16, value: '=', type: type.operator },
-    { id: 17, value: '0', type: type.symbol  },
-    { id: 18, value: '.', type: type.symbol  },
+        { id: 1, value: 'ac', type: type.special },
+        { id: 2, value: '<', type: type.special },
+        { id: 3, value: '/', type: type.operator },
+        { id: 4, value: 'x', type: type.operator },
+        { id: 5, value: '7', type: type.numbers },
+        { id: 6, value: '8', type: type.numbers },
+        { id: 7, value: '9', type: type.numbers },
+        { id: 8, value: '-', type: type.operator },
+        { id: 9, value: '4', type: type.numbers },
+        { id: 10, value: '5', type: type.numbers },
+        { id: 11, value: '6', type: type.numbers },
+        { id: 12, value: '+', type: type.operator },
+        { id: 13, value: '1', type: type.numbers },
+        { id: 14, value: '2', type: type.numbers },
+        { id: 15, value: '3', type: type.numbers },
+        { id: 16, value: '=', type: type.operator },
+        { id: 17, value: '0', type: type.numbers },
+        { id: 18, value: '.', type: type.special },
     ])
 
     return (
         <div className='buttons'>
-        {
-            buttons.map( (state: any): any => <Button {...state} />)
-        }     
+            {
+                buttons.map((state: any): any => <Button {...state} />)
+            }
         </div>
     )
 }
--- src/store/slices/Slices.ts
+++ src/store/slices/Slices.ts
@@ -15,14 +15,16 @@ interface ReturnState {
 const checkOperatorRepeat = (stateValue: string, stateHistory: string, result: string, actionValue: string): ReturnState => {
     if (stateValue[stateValue.length - 1] === '+' || stateValue[stateValue.length - 1] === '-' ||
         stateValue[stateValue.length - 1] === '/' || stateValue[stateValue.length - 1] === 'x') {
-        console.log(result.slice(0, -1))
+        console.log(result.slice(0, -2))
+
         result = result.slice(0, -1) + (actionValue === 'x' ? '*' : actionValue);
-        stateValue = stateValue.slice(0, -1) + (actionValue === 'x' ? '*' : actionValue);
-        stateHistory = stateHistory.slice(0, -1) + (actionValue === 'x' ? '*' : actionValue);
+        stateValue = stateValue.slice(0, -1) + actionValue;
+        stateHistory = stateHistory.slice(0, -1) + actionValue;
     } else {
+
         result += (actionValue === 'x' ? '*' : actionValue);
-        stateValue += (actionValue === 'x' ? '*' : actionValue);
-        stateHistory += (actionValue === 'x' ? '*' : actionValue);
+        stateValue += actionValue;
+        stateHistory += actionValue;
     }
     return {
         result,
@@ -39,18 +41,11 @@ export const slice = createSlice({
             const value = action.payload.value;
 
             switch (action.payload.type) {
-                case 'symbol':
+                case 'numbers':
                     if (state.value === '0') {
-                        if (value === '.') {
-                            state.result += ',';
-                            state.value += value;
-                            state.history += value;
-                        } else {
-                            state.result = value;
-                            state.value = value;
-                            state.history = value;
-
-                        }
+                        state.result = value;
+                        state.value = value;
+                        state.history = value;
                     } else {
                         state.result += value;
                         state.value += value;
@@ -62,39 +57,43 @@ export const slice = createSlice({
                         state.result = eval(state.result);
                         state.value = state.result;
                         state.history += `${value}${state.result}`;
-                        break;
                     }
                     else {
-                        state.value = checkOperatorRepeat(state.value, state.history, state.result, value).stateValue;
-                        state.history = checkOperatorRepeat(state.value, state.history, state.result, value).stateHistory;
-                        state.result = checkOperatorRepeat(state.value, state.history, state.result, value).result;
+                        let newState = checkOperatorRepeat(state.value, state.history, state.result, value);
+                        state.value = newState.stateValue;
+                        state.history = newState.stateHistory;
+                        state.result = newState.result;
                     }
-
-
-                    state.result += value;
-                    state.value += `${value}`
-                    state.history += `${value}`;
                     break;
                 case 'special':
-                    if (value === 'ac') {
-                        state.value = '0';
-                        state.history = '0'
-                    } else if (value === '<') {
-                        debugger
-                        if (state.value.length === 1) {
+                    switch (value) {
+                        case 'ac':
                             state.value = '0';
-                        } else {
-                            state.value = state.value.slice(0, state.value.length - 1);
-                        }
+                            state.history = '0'
+                            break;
+                        case '<':
+                            if (state.value.length === 1) {
+                                state.value = '0';
+                            } else {
+                                state.value = state.value.slice(0, state.value.length - 1);
+                            }
+                            break;
+                        case '.':
+                            if (state.value.slice(-1) === '.') {
+                                break;
+                            } else {
+                                state.result += ',';
+                                state.value += value;
+                                state.history += value;
+                            }
+                            break;
+                        default:
                     }
-                    break
+                    break;
                 default:
             }
-
-
         }
     }
-
 })
 
 export const { updateDisplayValue } = slice.actions;
